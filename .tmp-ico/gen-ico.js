05ecad8b758acb3e9d237f4325bf1d31d2f965eb
LengxiQwQ <lengxiowo@gmail.com>
2026-07-30 15:15:54 +0800

    chore: clean up backup, temp, and junk files from repo

--- .tmp-ico/gen-ico.js
+++
@@ -1,8 +0,0 @@
-
-const pngToIco = require("./node_modules/png-to-ico");
-const fs = require("fs");
-const files = ["D:\\Projects\\live-photo-box\\.tmp-ico\\icon-16.png", "D:\\Projects\\live-photo-box\\.tmp-ico\\icon-32.png", "D:\\Projects\\live-photo-box\\.tmp-ico\\icon-48.png", "D:\\Projects\\live-photo-box\\.tmp-ico\\icon-64.png", "D:\\Projects\\live-photo-box\\.tmp-ico\\icon-128.png", "D:\\Projects\\live-photo-box\\.tmp-ico\\icon-256.png"];
-pngToIco(files).then(buf => {
-    fs.writeFileSync("Live Photo Box/Assets/Icons/AppIcon.ico", buf);
-    console.log("ICO written: " + buf.length + " bytes, " + files.length + " sizes");
-}).catch(e => console.error("ERROR:", e));
